6a2e72b2bd941d2a640d1a27052be4971ed4aa24
Maite <maite@Maites-iMac.local>
2020-05-13 18:27:06 -0300

    work and services

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function($){
+    let typed = new Typed('.txt-rotate', {
+        strings: ['I\'m a Web Developer','I\'m a FrontEnd Developer','I\'m a QA Automation Analyst'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        fadeOut: false,
+        smartBackspace: true,
+        loop: true
+    });
+})(jQuery);
+
